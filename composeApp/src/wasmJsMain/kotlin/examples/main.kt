e4781d57f369a2ac4bf928c88e369ff4bf3185b5
Soufiane Hamama <soufianehamama9@gmail.com>
2023-12-17 22:05:21 +0100

    Managing packages

---
+++ composeApp/src/wasmJsMain/kotlin/examples/main.kt
@@ -0,0 +1,16 @@
+package examples
+
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.window.CanvasBasedWindow
+import com.hamama.kwhi.LocalLayerContainer
+import kotlinx.browser.document
+
+@OptIn(ExperimentalComposeUiApi::class)
+fun main() {
+    CanvasBasedWindow(canvasElementId = "ComposeTarget", title = "Kotlin Wasm Html Interop") {
+        CompositionLocalProvider(LocalLayerContainer provides document.body!!) {
+            App()
+        }
+    }
+}
